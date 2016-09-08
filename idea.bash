78ab54819b5561f42d7a8be25dfd4a2d61e3746a
martin.trummer <martin.trummer@tmtron.com>
2016-09-08 09:58:25 +0200

    idea.bash * added more comments * passing ANDROID_HOME env-var * using detached mode

--- idea.bash
+++ idea.bash
@@ -9,7 +9,19 @@ docker run \
   -v $XSOCK:$XSOCK \
   -v /home/$USER:/home/$USER \
   -e DISPLAY \
-  -it \
+  -e ANDROID_HOME \ 
+  -d \
   idea \
   /opt/intellij/bin/idea.sh
+
+# ANDROID_HOME is required because we may need it for android development/testing
+
+# -d detach: runs the container in the background and prints out the container id
+
+# when you want to run e.g. bash directly (instead of idea.sh), you must remove the -d switch and add: 
+#  -t alocate a pseudo-tty
+#  -i interactive: keep STDIN open iven if not attached
+
+
+
 
